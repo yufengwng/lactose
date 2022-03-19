f73cd877b49c4f046c79129a1f8592c0a6ad59fb
Yufeng Wang <yufengwang05@gmail.com>
2022-03-19 10:32:33 -0500

    lt: setup main command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
+use clap::Parser;
+use clap::Subcommand;
 use ltrepl::repl;
 
+#[derive(Parser)]
+#[clap(version)]
+#[clap(about = "the lactose language")]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Start the interactive command line prompt
+    Repl,
+}
+
 fn main() {
+    let cli = Cli::parse();
+    match cli.cmd {
+        Some(cmd) => handle(cmd),
+        None => handle_default(),
+    }
+}
+
+fn handle(cmd: Command) {
+    match cmd {
+        Command::Repl => run_repl(),
+    }
+}
+
+fn handle_default() {
+    run_repl();
+}
+
+fn run_repl() {
     println!("[lactose-lang]");
     match repl::start() {
         Ok(_) => (),
